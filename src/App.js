0dd5e9697d00f84ebcd2337bcb75e51f3204687f
ahmed-razic <ahmed.razic@gmail.com>
2022-03-02 23:46:22 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import NavBar from './components/NavBar/NavBar';
 import Home from './pages/Home/Home';
 import PastLaunches from './pages/PastLaunches/PastLaunches';
 import Rocket from './pages/Rocket/Rocket';
+import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
 
 function App() {
-  return (
-    <>
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/launches' element={<PastLaunches />} />
-          <Route path='/rocket/:id' element={<Rocket />} />
-        </Routes>
-      </Router>
-    </>
-  );
+    const client = new ApolloClient({
+        uri: 'https://api.spacex.land/graphql',
+        cache: new InMemoryCache(),
+    });
+
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+                <NavBar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/launches" element={<PastLaunches />} />
+                    <Route path="/rocket/:id" element={<Rocket />} />
+                </Routes>
+            </Router>
+        </ApolloProvider>
+    );
 }
 
 export default App;
