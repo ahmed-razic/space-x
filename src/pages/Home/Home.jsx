cfff2d6bcfb18fd92a536408dde41be404067ccf
ahmed-razic <ahmed.razic@gmail.com>
2022-03-03 12:08:09 +0100

    pastlaunches

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,29 @@
 import './style.scss';
+import { gql, useQuery } from '@apollo/client';
+import Error from '../../components/Error';
+import Loader from '../../components/Loader';
+import MainHeader from '../../components/MainHeader';
+
+const GET_COMPANY_INFO = gql`
+    {
+        company {
+            name
+            summary
+        }
+    }
+`;
 
 function Home() {
-  return (
-    <div className='home__container d-flex align-items-center text-center'></div>
-  );
+    const { data, loading, error } = useQuery(GET_COMPANY_INFO);
+
+    if (loading) return <Loader />;
+    if (error) return <Error />;
+
+    return (
+        <div className="home__container d-flex align-items-center text-center">
+            <MainHeader name={data.company.name} description={data.company.summary} />
+        </div>
+    );
 }
 
 export default Home;
--- src/pages/PastLaunches/PastLaunches.jsx
+++ src/pages/PastLaunches/PastLaunches.jsx
@@ -1,11 +1,40 @@
 import { Fragment } from 'react';
+import { gql, useQuery } from '@apollo/client';
+import Error from '../../components/Error';
+import Loader from '../../components/Loader';
+import LaunchesFeed from '../../components/LaunchesFeed';
+
+const GET_LAUNCHES_QUERY = gql`
+    {
+        launchesPast(limit: 15) {
+            mission_name
+            launch_site {
+                site_name_long
+            }
+            links {
+                article_link
+                flickr_images
+            }
+            id
+        }
+    }
+`;
 
 function PastLaunches() {
-  return (
-    <Fragment>
-      <h1 className='display-4 text-center my-5 pt-5'>Past Launches</h1>
-    </Fragment>
-  );
+    const { data, error, loading } = useQuery(GET_LAUNCHES_QUERY);
+    if (loading) return <Loader />;
+    if (error) return <Error />;
+
+    const launches = data.launchesPast.filter(
+        launch => launch.links.article_link && launch.links.flickr_images.length > 0
+    );
+
+    return (
+        <Fragment>
+            <h1 className="display-4 text-center my-5 pt-5">Past Launches</h1>
+            <LaunchesFeed launches={launches} />
+        </Fragment>
+    );
 }
 
 export default PastLaunches;
