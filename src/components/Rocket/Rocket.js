f629bcb1bb507a93d031457918ad411adf1f26b2
ahmed-razic <ahmed.razic@gmail.com>
2022-03-03 13:19:05 +0100

    useTechInfo hook

--- src/components/Rocket/Rocket.js
+++ src/components/Rocket/Rocket.js
@@ -1,11 +1,17 @@
 import React, { Fragment } from 'react';
-import RocketHeader from "../../components/RocketHeader"
-import TechInfo from "../../components/TechInfo"
-
+import RocketHeader from '../../components/RocketHeader';
+import TechInfo from '../../components/TechInfo';
 
 const Rocket = ({ rocket }) => {
-    {id, name, height, diameter, stages, cost_per_launch, engines} = rocket
-    return <Fragment></Fragment>;
+    const { id, name, height, diameter, stages, cost_per_launch, engines } = rocket;
+    return (
+        <Fragment>
+            <div className="rocket dflex flex-column align-items-center justify-content-around">
+                <RocketHeader id={id} name={name} />
+                <TechInfo name={name} rocket={{ height, diameter, stages, cost: cost_per_launch }} />
+            </div>
+        </Fragment>
+    );
 };
 
 export default Rocket;
